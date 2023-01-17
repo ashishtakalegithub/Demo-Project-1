db51fe7d91e5d9bbf8bece1725429c66be2f2525
Ashish <ashishtakale@gmail.com>
2023-01-17 23:52:35 +0530

    AddedArrayListDemo

---
+++ src/com/velocity/Test.java
@@ -0,0 +1,15 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ArrayList<String> al=new ArrayList<String>();
+		al.add("java");
+		al.add("hibernate");
+		al.add("spring");
+		System.out.println(al);
+	}
+
+}
